cf7e33da0d43d6428d64a6aaa5dd5452a59fdf5f
Görkem <emircan9921@outlook.com>
2022-12-29 15:57:19 +0300

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,3 @@
-/*
- * Emircan Görkem ECE - 210303049
- * Ebrar Esila Mutlu - 190303066
- */
-
 public class Server {
     public static void main(String[] args) throws InterruptedException {
         ExecuteServer e = new ExecuteServer();
